90df30ec539ddda4706806c8f530f5b28512b81b
Jonathan Jacka <jonathanjacka@gmail.com>
2022-07-06 09:51:12 -0500

    Added basic CRUD routes

--- server.js
+++ server.js
@@ -1,21 +1,60 @@
 const express = require('express');
 const app = express();
 //const {Restaurant} = require("./models/index")
-const data = require('./seedData');
+let data = require('./seedData');
 
 const port = 3000;
 
 //TODO: Create your GET Request Route Below:
 app.use(express.json());
 
+//GET ALL
 app.get('/restaurants', (req, res) => {
   res.status(200).json(data);
 });
 
+//GET ONE BY ID
 app.get('/restaurants/:id', (req, res) => {
   const productId = Number(req.params.id);
   const result = data.filter((product) => product.id === productId);
   res.status(200).json(result);
+});
+
+//UPDATE ONE BY ID
+app.put('/restaurants/:id', (req, res) => {
+  const productId = Number(req.params.id);
+  let index;
+
+  data.forEach((item) => {
+    if (item.id === productId) {
+      index = data.indexOf(item);
+    }
+  });
+
+  data[index] = { ...data[index], name: 'New Name' };
+
+  res.status(200).json(data);
+});
+
+//CREATE
+app.post('/restaurants', (req, res) => {
+  const id = Math.floor(Math.random() * 1000000000);
+  const newItem = {
+    name: "Jonathan's Pizza",
+    location: 'Chicago',
+    cuisine: 'Fast Food',
+    id,
+  };
+  data.push(newItem);
+  res.status(200).json(data);
+});
+
+//DELETE
+app.delete('/restaurants/:id', (req, res) => {
+  const productId = Number(req.params.id);
+  const newData = data.filter((product) => product.id != productId);
+  data = [...newData];
+  res.status(200).json(data);
 });
 
 app.listen(port, () => {
