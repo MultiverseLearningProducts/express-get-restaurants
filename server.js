be5d39085d417904cd985dff5eb2835818d725a5
Jonathan Jacka <jonathanjacka@gmail.com>
2022-07-06 09:09:05 -0500

    Udated GET restaurants

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ const data = require('./seedData');
 const port = 3000;
 
 //TODO: Create your GET Request Route Below:
+app.use(express.json());
 
-console.log('Data: ', data);
+app.get('/restaurants', (req, res) => {
+  res.status(200).json(data);
+});
 
 app.listen(port, () => {
   console.log('Your server is listening on port ' + port);
