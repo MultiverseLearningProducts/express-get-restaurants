652fcb6824922cb40c3f5a92caffefd528b9e485
Alex Evans <aevans@quiktrip.com>
2024-02-21 15:37:29 -0600

    added new files

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const Restaurant = require("../models/index")
 const db = require("../db/connection");
+const restaurantRoute = require('../models/Restaurant')
+app.use()
 app.use(express.json());
 app.use(express.urlencoded());
 
