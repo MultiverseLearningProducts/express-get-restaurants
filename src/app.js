ab57e52c5a757f444371980877134b73b556997d
eviecutrell <evelynn.cutrell@alaskaair.com>
2024-02-22 13:49:28 -0800

    test issues

--- src/app.js
+++ src/app.js
@@ -10,7 +10,28 @@ app.get("/restaurants", async (request, response) => {
     response.json(restaurants);
 });
 
+app.get("/restaurants/:id", async (req, res) => {
+    const restaurant = await Restaurant.findByPk(req.params.id);
+    res.json(restaurant);
+});
 
+app.use(express.json());
+app.use(express.urlencoded());
 
+app.post("/restaurants", async (req, res) => {
+    const restaurant = await Restaurant.create(req.body);
+    const allRestaurant = await Restaurant.findAll();
+    res.json(allRestaurant);
+});
+
+app.put("/restaurants/:id", async (req, res) => {
+    const restaurant = await Restaurant.update(req.body, {where: {id: req.params.id}});
+    res.json(restaurant);
+});
+
+app.delete("/restaurants/:id", async (req, res) => {
+    const delRestaurant = await Restaurant.destroy({where: {id: req.params.id}});
+    res.json(delRestaurant);
+});
 
 module.exports = app;
