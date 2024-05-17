3ca35ff6606bde7ea8d6247946e3a0e12d6a3916
Bridget Galang <galanbri@gmail.com>
2024-05-17 13:31:27 -0700

    create get request for restaurants

--- src/app.js
+++ src/app.js
@@ -4,7 +4,14 @@ const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
-
+app.get('/restaurants', async (req, res) => {
+    try {
+        const restaurants = await Restaurant.findAll({});
+        res.json(restaurants);
+    } catch (error) {
+        console.error(error);
+    }
+})
 
 
 
